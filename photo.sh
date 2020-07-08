aea7edc0edfb85977bd7359c84372f05b4f4abe8
reid23 <40401995+reid23@users.noreply.github.com>
2020-07-08 10:53:11 -0700

    Create photo.sh

---
+++ photo.sh
@@ -0,0 +1,4 @@
+sudo kill -9 `pidof mjpg_streamer`
+DATE=$(date +"%Y-%m-%d_%H:%M:%S")
+raspistill -o /var/www/html/photos/$DATE.jpg
+sudo /usr/local/bin/mjpg_streamer -i "input_uvc.so -r 1280x720 -d /dev/video0 -f 30 -q 80" -o "output_http.so -p 8080 -w /usr/local/share/mjpg-streamer/www"
