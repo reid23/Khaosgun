550136278e213345e01e551c180dbc82f0b7d6c9
Reid Dye <reiddye@Reids-MacBook-Pro.local>
2020-08-24 17:12:32 -0700

    AI WORKSgit status! still just uses a portion of the frame, no yolo coordinates

--- detectHuman.sh
+++ detectHuman.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /Users/reiddye/darknet
-./darknet detect cfg/yolov3-tiny.cfg yolov3-tiny.weights data/person.jpg
+./darknet detect cfg/yolov3-tiny.cfg yolov3-tiny.weights data/mjpgStream.jpg
 cd
--- laptopScript.py
+++ laptopScript.py
@@ -4,27 +4,40 @@ import time
 from PIL import Image
 import tempfile
 import subprocess
+import urllib.request
+import requests
+
+def req(url):
+    r = requests.get(url)
+    return r.text
+
+def url_is_alive(url):
+    """
+    Checks that a given URL is reachable.
+    :param url: A URL
+    :rtype: bool
+    """
+    request = urllib.request.Request(url)
+    request.get_method = lambda: 'HEAD'
+
+    try:
+        urllib.request.urlopen(request)
+        return True
+    except urllib.request.HTTPError:
+        return False
+
 def fire():
     print("shoot!!!!!!")
+    req("http://khaosgun.local/pinon.php")
+    time.sleep(0.8)  # this is how long the gun revs to get going before firing.
+    req("http://khaosgun.local/firepinon.php")
+    time.sleep(1.5)  # this is how long the gun is firing.
+    req("http://khaosgun.local/firepinoff.php")
+    req("http://khaosgun.local/pinoff.php")
 
 
 while True:
-    os.system("ffmpeg -y -i http://khaosgun.local:8080/?action=stream /Users/reiddye/darknet/data/mjpgStream.jpg")
-    im = Image.open("/Users/reiddye/darknet/data/mjpgStream.jpg")
-    width, height = im.size
-
-    # Setting the points for cropped image
-    left = 1070
-    top = 991
-    right = 1415
-    bottom = 1286
-
-    # Cropped image of above dimension
-    im = im.crop((left, top, right, bottom))
-
-    # Shows the image in image viewer
-    im.save("/Users/reiddye/darknet/data/mjpgStream.jpg")
-
+    os.system("wget http://khaosgun.local:8080/?action=snapshot -O /Users/reiddye/darknet/data/mjpgStream.jpg")
     #Mink
     #Meerkat
     #Weasel
@@ -33,14 +46,33 @@ while True:
     #Marmot
     #wood_rabbit
     with tempfile.TemporaryFile() as tempf:
-        human = http.client.HTTPConnection("khaosgun.local/human.html")
-        squirrel = http.client.HTTPConnection("khaosgun.local/squirrel.html")
-        if(squirrel.getresponse().status == 200):
+        if(url_is_alive("http://khaosgun.local/squirrel.html")):
+            im = Image.open("/Users/reiddye/darknet/data/mjpgStream.jpg")
+            width, height = im.size
+
+            # Setting the points for cropped image
+
+            # oall x: 1280
+            # oall y: 720
+            #2556 × 1440
+
+            top = (850/2556) * width  # x1
+            left = (1070/1440) * height  # y1
+
+            right = (1415/2556) * width  # x2
+            bottom = (1286/1440) * height  # y2
+
+            # Cropped image of above dimension
+            im = im.crop((left, top, right, bottom))
+
+            # Shows the image in image viewer
+            im.save("/Users/reiddye/darknet/data/mjpgStream.jpg")
             proc = subprocess.Popen(['./detectSquirrel.sh'], stdout=tempf)
             proc.wait()
             tempf.seek(0)
             print(tempf.read())
             result = tempf.read()
+            result = str(result)
             if(result.find("fox_squirrel") != -1):
                 numberIndex = result.find("fox_squirrel")-8
                 result = result[numberIndex:numberIndex+5]
@@ -98,19 +130,39 @@ while True:
                     fire()
             else:
                 pass
-        elif(human.getresponse().status == 200):
+        elif(url_is_alive("http://khaosgun.local/human.html")):
+            im = Image.open("/Users/reiddye/darknet/data/mjpgStream.jpg")
+            width, height = im.size
+
+             # Setting the points for cropped image
+            top = (250/2556) * width  # x1
+            left = (800/1440) * height  # y1
+
+            right = (1665/2556) * width  # x2
+            bottom = (1440/1440) * height  # y2
+
+            # Cropped image of above dimension
+            im = im.crop((left, top, right, bottom))
+
+            # saves the cropped image
+            im.save("/Users/reiddye/darknet/data/mjpgStream.jpg")
+
+
             proc = subprocess.Popen(['./detectHuman.sh'], stdout=tempf)
             proc.wait()
             tempf.seek(0)
             result = tempf.read()
-            numberIndex = result.find("person")
+            result = str(result)
+            print(result)
+            numberIndex = result.find('person')
             if(numberIndex != -1):
                 numberIndex += 8
                 result = result[numberIndex:numberIndex+2]
                 if(result.find("%") != -1):
                     result = result[:1]
-                if(float(result)<20): #change the 20 for changing sensetivity
+                if(float(result)>20): #change the 20 for changing sensetivity
                     fire()
+                print(result)
                 
         else:
-            time.sleep(0.1)
+            pass
